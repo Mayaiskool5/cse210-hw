e4304cf71392165185004f8fbfd76d370845c4d5
Use your GitHub username <jer14002@byui.edu>
2023-04-25 09:53:24 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> num = new List<int>();
+        
+        int userNum = -1;
+        while (userNum != 0)
+        {
+            Console.Write("Enter a number (0 to end): ");
+            
+            string userResp = Console.ReadLine();
+            userNum = int.Parse(userResp);
+
+            if (userNum != 0)
+            {
+                num.Add(userNum);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in num)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / num.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = num[0];
+
+        foreach (int number in num)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
